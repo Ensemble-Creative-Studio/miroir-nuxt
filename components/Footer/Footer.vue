c003ddfec58ec98f70289314fa897abfe6a38a64
victorcngo <hello@victorcango.fr>
2023-08-05 15:14:22 +0200

    feat(components): create Loader and Footer

---
+++ components/Footer/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <footer class="Footer">
+    <ul>
+      <li>
+        <NuxtLink to="/legal">Legal</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/cookies">Cookies</NuxtLink>
+      </li>
+    </ul>
+  </footer>
+</template>
+
+<style lang="scss">
+.Footer {
+  margin-top: 15rem;
+  padding-bottom: 1.5rem;
+  text-transform: uppercase;
+  font-size: 1.4rem;
+
+  ul {
+    display: flex;
+    justify-content: center;
+
+    li:not(:last-child) {
+      margin-right: 2rem;
+    }
+  }
+}
+</style>
--- components/Loader/Loader.vue
+++ components/Loader/Loader.vue
@@ -1,3 +1,80 @@
+<script setup>
+import { useLoaderStore } from '@/stores/loader'
+const store = useLoaderStore()
+
+const currentIndex = ref(0)
+const SPEED = 1000
+
+let interval = setInterval(() => {
+  currentIndex.value++
+}, SPEED)
+
+const goForward = () => {
+  currentIndex.value++
+}
+
+watch(currentIndex, (value) => {
+  if (value === 5) {
+    clearInterval(interval)
+
+    setTimeout(() => {
+      store.endLoader()
+    }, SPEED)
+  }
+})
+</script>
+
 <template>
-  <div></div>
+  <div class="Loader" @click="goForward()">
+    <div
+      class="image"
+      :class="index > currentIndex ? 'image--hidden' : ''"
+      ref="$image"
+      :style="{ zIndex: index }"
+      v-for="(n, index) in 6"
+    >
+      <img :src="'img/' + index + '.jpg'" />
+    </div>
+  </div>
 </template>
+
+<style scoped lang="scss">
+.Loader {
+  position: fixed;
+  overflow: hidden;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100%;
+  z-index: 30;
+  cursor: pointer;
+
+  .image {
+    height: 100vh;
+    width: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    transition: opacity 0.5s ease-in-out;
+
+    &--hidden {
+      opacity: 0;
+    }
+
+    img {
+      object-fit: cover;
+      object-position: center;
+      height: 100%;
+    }
+  }
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease-in-out;
+}
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
+</style>
