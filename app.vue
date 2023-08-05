baec2adb8c1fa28c1ef9b93b07f25a14c199a5b7
victorcngo <hello@victorcango.fr>
2023-08-05 15:15:35 +0200

    fix(plugins): remove lenis to instantiate it once

--- app.vue
+++ app.vue
@@ -1,6 +1,46 @@
+<script setup>
+import Lenis from '@studio-freight/lenis'
+
+import { useLoaderStore } from '@/stores/loader'
+const store = useLoaderStore()
+
+let lenis
+let raf
+
+const initLenis = () => {
+  lenis = new Lenis()
+
+  raf = (time) => {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+  }
+  requestAnimationFrame(raf)
+}
+
+onMounted(() => {
+  initLenis()
+})
+
+const route = useRoute()
+
+if (route.fullPath !== '/') {
+  store.endLoader()
+}
+
+watch(
+  () => route.fullPath,
+  () => {
+    lenis.scrollTo(0, {
+      immediate: true,
+    })
+  }
+)
+</script>
+
 <template>
   <div>
     <Header />
     <NuxtPage />
+    <Footer />
   </div>
 </template>
